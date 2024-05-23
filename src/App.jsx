900a8cf18f62d8b24b94c61914d023008f1384b9
Samuel Muli <samuelmuli95@gmail.com>
2024-05-23 20:04:58 +0300

    Initialize home component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import SignIn from './pages/SignIn.jsx'
 import AuthProvider from './store/AuthContext'
 import Orders from './pages/Orders.jsx'
 import ProtectedRoute from './components/ProtectedRoute.jsx'
+import Home from './pages/Home.jsx'
 
 function App() {
 
   return (
     <AuthProvider>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<SignIn />} />
         <Route path="/orders" element={<ProtectedRoute component={Orders} />} />
